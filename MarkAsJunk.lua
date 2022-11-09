908a1326b5569899ee9a937da4f8a14c049fb1f2
Gabriel N. Lujan <anewlevelmp@gmail.com>
2022-11-09 13:18:17 -0800

    feat(main): add basic condition around start-up greeting;

--- MarkAsJunk.lua
+++ MarkAsJunk.lua
@@ -1,2 +1,5 @@
-local name = UnitName("player");
-print('Hey, ' .. name .. '! Thanks for using "Mark As Junk"! Type "/maj" to get more info.');
+-- update this condition to be based off of a UI setting so that it disables this greeting
+if true then
+    local name = UnitName("player");
+    print('Hi, ' .. name .. '! Thanks for using "Mark As Junk"! Type "/maj" to get more info.');
+end
