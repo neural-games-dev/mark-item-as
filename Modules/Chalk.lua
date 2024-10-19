2fd1c005a06e5b2af3546e633cf248cf627a09c2
Metal Gabe <anewlevelmp@gmail.com>
2024-10-19 16:45:03 -0700

    chore(tww-refactor): reorganize & update Chalk methods;

--- Modules/Chalk.lua
+++ Modules/Chalk.lua
@@ -12,6 +12,28 @@ local Chalk = MarkItemAs:NewModule('Chalk');
 --## ===============================================================================================
 --## DEFINING ALL TEXT COLORIZING UTILS TO BE USED THROUGHOUT THE ADDON
 --## ===============================================================================================
+-- ——————————————————————————————————————————
+-- TRADITIONAL LOG METHODS
+-- ——————————————————————————————————————————
+function Chalk:debug(text)
+   return '|cFFfd4a4a' .. text .. '|r';
+end
+
+function Chalk:error(text)
+   return '|cFFff0000' .. text .. '|r';
+end
+
+function Chalk:info(text)
+   return '|cFF0000ff' .. text .. '|r';
+end
+
+function Chalk:warn(text)
+   return '|cFFfa8200' .. text .. '|r';
+end
+
+-- ——————————————————————————————————————————
+-- COLORS
+-- ——————————————————————————————————————————
 function Chalk:ace(text)
    return '|cFF33ff99' .. text .. '|r';
 end
@@ -28,10 +50,6 @@ function Chalk:cyan(text)
    return '|cFF00ffff' .. text .. '|r';
 end
 
-function Chalk:debug(text)
-   return '|cFFfd4a4a' .. text .. '|r';
-end
-
 function Chalk:gray(text)
    return '|cFFb0b0b0' .. text .. '|r';
 end
@@ -46,8 +64,4 @@ end
 
 function Chalk:red(text)
    return '|cFFfd4a4a' .. text .. '|r';
-end
-
-function Chalk:warn(text)
-   return '|cFFfa8200' .. text .. '|r';
 end
